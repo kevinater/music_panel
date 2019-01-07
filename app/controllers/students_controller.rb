143dbcd08036c1a2516f746e90c8764db66881d0
Kevin Vahdat <kvahdat@nyc.yearup.org>
2019-01-07 13:52:27 -0500

    Created all controllers and tables

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,22 @@
+class StudentsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
