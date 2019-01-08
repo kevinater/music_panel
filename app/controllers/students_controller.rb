0f71284a0949d680f52ac8dc1ba0220c27e42abd
Kevin Vahdat <kvahdat@nyc.yearup.org>
2019-01-08 16:24:43 -0500

    Created CRUD operations for student class

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,20 +5,46 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params:[id])
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.create(student_params)
+    if @student.save
+      redirect_to students_path
+    else
+      render :new
+    end
   end
 
   def update
+    @student = Student.find(params[:id])
+      if @student.update(student_params)
+        @student.save
+        redirect_to students_path
+      else
+        render :edit
+      end
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to students_path
   end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :age, :education)
+  end
+
 end
